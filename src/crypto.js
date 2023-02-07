879bc39f7a2bbfc42211dd090090fbe776f26bd8
yerundong <414868970@qq.com>
2023-02-07 10:56:56 +0800

    增加base64url处理方法

--- src/crypto.js
+++ src/crypto.js
@@ -1,55 +1,99 @@
-import CryptoJS from 'crypto-js'
+import CryptoJS from "crypto-js";
 
 /**
  * @description 生成uuid，根据随机数，或者伪随机数生成UUID，有一定的重复概率
  * @param { Number } len 长度
- * @param { Number } radix 基数 
+ * @param { Number } radix 基数
  */
 export const uuid = (len, radix) => {
-  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')
-  const uuid = []; let i
-  radix = radix || chars.length
+  const chars =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
+  const uuid = [];
+  let i;
+  radix = radix || chars.length;
 
   if (len) {
     // Compact form
-    for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix]
+    for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)];
   } else {
     // rfc4122, version 4 form
-    let r
+    let r;
 
     // rfc4122 requires these characters
-    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-'
-    uuid[14] = '4'
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = "-";
+    uuid[14] = "4";
 
     // Fill in random data.  At i==19 set the high bits of clock sequence as
     // per rfc4122, sec. 4.1.5
     for (i = 0; i < 36; i++) {
       if (!uuid[i]) {
-        r = 0 | Math.random() * 16
-        uuid[i] = chars[(i === 19) ? (r & 0x3) | 0x8 : r]
+        r = 0 | (Math.random() * 16);
+        uuid[i] = chars[i === 19 ? (r & 0x3) | 0x8 : r];
       }
     }
   }
 
-  return uuid.join('')
-}
+  return uuid.join("");
+};
 
-/**  
+/**
  * @description: base64加密
  * @param {String} word 需要加密的字符串
  */
 export const base64Encrypt = (word) => {
-  const wordArray = CryptoJS.enc.Utf8.parse(word)
-  const base64 = CryptoJS.enc.Base64.stringify(wordArray)
-  return base64
-}
+  const wordArray = CryptoJS.enc.Utf8.parse(word);
+  const base64 = CryptoJS.enc.Base64.stringify(wordArray);
+  return base64;
+};
 
 /**
  * @description: base64解密
  * @param {String} word 需要解密的字符串
  */
 export const base64Decrypt = (word) => {
-  const wordArray = CryptoJS.enc.Base64.parse(word)
-  const deBase64 = wordArray.toString(CryptoJS.enc.Utf8)
-  return deBase64
-}
+  const wordArray = CryptoJS.enc.Base64.parse(word);
+  const deBase64 = wordArray.toString(CryptoJS.enc.Utf8);
+  return deBase64;
+};
+
+/**
+ * @description: 得到base64数据，补全前缀变成base64的url，如：data:image/png;base64,……
+ * @param {String} format 文件格式，如png、pdf
+ * @param {String} base64Data base64数据
+ */
+export const completeBase64Url = (format, base64Data) => {
+  let formt = format.toLowerCase();
+  let type = "";
+  if (["png", "jpg", "jpeg", "gif", "svg", "webp"].includes(formt)) {
+    type = "image";
+    if (formt === "jpg") {
+      formt === "jpeg";
+    } else if (formt === "svg") {
+      formt === "svg+xml";
+    }
+  } else if (["pdf", "doc", "docx", "ppt"].includes(formt)) {
+    type = "application";
+    if (formt === "doc") {
+      formt === "msword";
+    } else if (formt === "docx") {
+      formt === "vnd.openxmlformats-officedocument.wordprocessingml.document";
+    } else if (formt === "ppt") {
+      formt === "vnd.ms-powerpoint";
+    }
+  } else if (["txt"].includes(formt)) {
+    type = "text";
+    if (formt === "txt") {
+      formt === "plain";
+    }
+  }
+  return `data:${type}/${formt};base64,${base64Data}`;
+};
+
+/**
+ * @description: 得到base64的Url，剔除base64Url的前缀，得到base64数据
+ * @param {String} base64Url base64的url
+ */
+export const removeBase64UrlPrefix = (base64Url) => {
+  const index = base64Url.search("base64,");
+  return base64Url.substring(index + 7);
+};
