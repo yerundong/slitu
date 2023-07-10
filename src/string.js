b377ede43b69dc6276eb3cccac817dbd3be85a76
yerundong <414868970@qq.com>
2023-07-10 10:55:55 +0800

    feat：增加strEllipsis

--- src/string.js
+++ src/string.js
@@ -7,3 +7,13 @@ export const trims = (value) => {
   checkTypeOrError(value, "String");
   return value.replace(/\s/g, "");
 };
+
+/**
+ * 字符串超过某个长度剔除，并加上省略号（用于多行省略）
+ */
+export const strEllipsis = (str, length) => {
+  if (str.length <= length) {
+    return str;
+  }
+  return str.slice(0, length - 1) + "…";
+};
