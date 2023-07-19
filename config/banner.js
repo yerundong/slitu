1c630bb26dd7ef06914588b98c358585cbe6411c
yerundong <414868970@qq.com>
2023-07-19 14:37:04 +0800

    Revert "feat：rollup打包"
    This reverts commit 2bcb022019b69159d1d0e3480ffcf105f20e95b7.

--- config/banner.js
+++
@@ -1,7 +0,0 @@
-import pkg from '../package.json'
-
-export default `${pkg.name} - v${pkg.version}
-${pkg.homepage}
-
-Copyright (c) ${new Date().getFullYear()} ${pkg.author}
-Released under MIT license`
