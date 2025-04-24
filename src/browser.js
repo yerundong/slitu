95952b9a505df5625359301bc8952e7d54258091
叶润东 <414868970@qq.com>
2025-04-24 15:53:51 +0800

    feat：2.0.0

--- src/browser.js
+++ src/browser.js
@@ -1,44 +1,58 @@
-import { checkTypeOrError, getTypeOf } from "./type";
+import { checkTypeOrError } from "./type";
 
 /**
- * 通过目标对象生成 URL search数据: "?a=1&b=2&c=3"
- * @param {Object} objValue 目标对象
- * @param {Boolean} encode 是否对每个参数进行编码
- * @param {Boolean} withPrefix 是否带?符号，默认true
+ * 通过 object 数据生成 URL 查询字符串: "?a=1&b=2&c=3"
+ * @param {Object} obj 目标对象
+ * @param options 配置
+ * @property {Boolean} prefix 生成的结果是否带?前缀，默认true
+ * @property {Boolean} encode 是否对每个参数的键和值进行编码，默认false
+ * @property {Boolean} stringify 是否对每个参数的值进行字符串化，默认false
  */
-export const queryStringify = (
-  objValue = {},
-  encode = true,
-  withPrefix = true
-) => {
-  checkTypeOrError(objValue, "Object");
-  checkTypeOrError(encode, "Boolean");
-  checkTypeOrError(withPrefix, "Boolean");
-  let str = withPrefix ? "?" : "";
-  for (let key in objValue) {
-    const value = objValue[key];
-    let value_;
-    if (getTypeOf(value) === "basic") {
-      value_ = encode ? encodeURIComponent(value) : value;
-    } else {
-      value_ = encode
-        ? encodeURIComponent(JSON.stringify(value))
-        : JSON.stringify(value);
-    }
+export const stringifyQuery = (obj = {}, options = {}) => {
+  checkTypeOrError(obj, "Object");
+  checkTypeOrError(options, "Object");
+  const defOptions = {
+    prefix: true,
+    encode: false,
+    stringify: false,
+  };
+
+  const options_ = Object.assign({}, defOptions, options);
+  const { prefix, encode, stringify } = options_;
+
+  let str = prefix ? "?" : "";
+  for (let key in obj) {
+    const value = obj[key];
+    let KEY, VALUE;
+    KEY = encode ? encodeURIComponent(key) : key;
+    const valfy = stringify ? JSON.stringify(value) : value;
+    VALUE = encode ? encodeURIComponent(valfy) : valfy;
     if (str === "" || str === "?") {
-      str += `${key}=${value_}`;
+      str += `${KEY}=${VALUE}`;
     } else {
-      str += `&${key}=${value_}`;
+      str += `&${KEY}=${VALUE}`;
     }
   }
   return str;
 };
 
 /**
- * 解析 URL search数据，生成 object 数据
+ * 解析 URL 查询数据，生成 object 数据
+ * @param {Object} obj URL 查询字符串
+ * @param options 配置
+ * @property {Boolean} encode 是否对每个参数的键和值进行编码，默认false
+ * @property {Boolean} stringify 是否对每个参数的值进行逆字符串化，默认false
  */
-export const parseQueryString = (queryString = "") => {
+export const parseQuery = (queryString = "", options = {}) => {
   checkTypeOrError(queryString, "String");
+  checkTypeOrError(options, "Object");
+  const defOptions = {
+    encode: false,
+    stringify: false,
+  };
+  const options_ = Object.assign({}, defOptions, options);
+  const { encode, stringify } = options_;
+
   if (!queryString) return {};
   const arr1 = queryString.split("?");
   let queryStr;
@@ -49,18 +63,43 @@ export const parseQueryString = (queryString = "") => {
   } else {
     return {};
   }
-  const arr2 = decodeURIComponent(queryStr).split("&");
+  const arr2 = queryStr.split("&");
   const obj = {};
   arr2.forEach((item) => {
-    const arr3 = item.split("=");
-    obj[arr3[0]] = arr3[1];
+    const [key, value] = item.split("=");
+    const KEY = encode ? decodeURIComponent(key) : key;
+    const deval = encode ? decodeURIComponent(value) : value;
+    const VALUE = stringify ? JSON.parse(deval) : deval;
+    obj[KEY] = VALUE;
   });
   return obj;
 };
 
 /**
- * 从浏览器url中解析search数据
+ * 从浏览器url中解析查询数据
+ * @param options 配置
+ * @property {String} from 原始数据来自：search-从search中解析（默认），hash-从hash解析，all-从search、hash中解析合并（相同字段，search会覆盖hash）
+ * @property {Boolean} encode 是否对每个参数的键和值进行编码，默认false
+ * @property {Boolean} stringify 是否对每个参数的值进行逆字符串化，默认false
  */
-export const getLocationSearch = () => {
-  return parseQueryString(location.search || "");
+export const getUrlQuery = (options = {}) => {
+  checkTypeOrError(options, "Object");
+  const defOptions = {
+    from: "search",
+    encode: false,
+    stringify: false,
+  };
+  const options_ = Object.assign({}, defOptions, options);
+  const { from } = options_;
+  const search = location.search || "";
+  const hash = location.hash || "";
+  let ser = {},
+    has = {};
+  if (from === "search" || from === "all") {
+    ser = parseQuery(search, options_);
+  }
+  if (from === "hash" || from === "all") {
+    has = parseQuery(hash.split("?")?.[1] ?? "", options_);
+  }
+  return Object.assign({}, has, ser);
 };
