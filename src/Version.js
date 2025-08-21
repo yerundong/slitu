7076010be5c42b2e1dc379f5b8fb5a67e713b800
叶润东 <414868970@qq.com>
2025-08-21 14:13:01 +0800

    fix

--- src/Version.js
+++ src/Version.js
@@ -47,93 +47,108 @@ class Version {
   }
   /**
    * @description 任意比较符号比较
-   * @param {String} symbol 比较符号，如>、<、≥、≤、lt、gte等
-   * @param {String|Version} version 版本号，如1.0.0或Version实例
+   * @param {String} symbol 比较符号，如>、<、≥、≤、lt、gte等，必传
+   * @param {String|Version} version 版本号，如1.0.0或Version实例，必传
    * @returns {Boolean} 返回比较结果，若实例版本和参数版本在对应符号意义的比较上成立，返回true，否则返回false
    */
   compareWith(symbol, version) {
-    checkTypeOrError(symbol, "symbol", "String");
+    checkRTAOrError(symbol, "symbol", true, ["String"]);
+    checkRTAOrError(version, "version", true, [["String", Version]]);
     let sym;
-    const sarr1 = [">", "<", "=", "≠", "≥", "≤"];
-    const sarr2 = ["gt", "lt", "eq", "neq", "gte", "lte"];
+    const sarr1 = ["gt", "lt", "eq", "neq", "gte", "lte"];
+    const sarr2 = [">", "<", "=", ["≠", "!="], ["≥", ">="], ["≤", "<="]];
+
     if (sarr1.includes(symbol)) {
-      const idx = sarr1.indexOf(symbol);
-      sym = sarr2[idx];
-    } else if (sarr2.includes(symbol)) {
       sym = symbol;
+    } else {
+      for (let i = 0, item; (item = sarr2[i]); i++) {
+        if (item === symbol || item?.includes(symbol)) {
+          sym = sarr1[i];
+          break;
+        }
+      }
     }
     if (sym) return this[sym](version);
     return false;
   }
   /**
    * @description 大于
-   * @param {String|Version} version 版本号，如1.0.0或Version实例
+   * @param {String|Version} version 版本号，如1.0.0或Version实例，必传
    * @returns {Boolean} 返回比较结果，若实例版本大于参数版本，返回true，否则返回false
    */
   gt(version) {
+    checkRTAOrError(version, "version", true, [["String", Version]]);
     return this.compare(version) > 0;
   }
   /**
    * @description 小于
-   * @param {String|Version} version 版本号，如1.0.0或Version实例
+   * @param {String|Version} version 版本号，如1.0.0或Version实例，必传
    * @returns {Boolean} 返回比较结果，若实例版本小于参数版本，返回true，否则返回false
    */
   lt(version) {
+    checkRTAOrError(version, "version", true, [["String", Version]]);
     return this.compare(version) < 0;
   }
   /**
    * @description 等于
-   * @param {String|Version} version 版本号，如1.0.0或Version实例
+   * @param {String|Version} version 版本号，如1.0.0或Version实例，必传
    * @returns {Boolean} 返回比较结果，若实例版本等于参数版本，返回true，否则返回false
    */
   eq(version) {
+    checkRTAOrError(version, "version", true, [["String", Version]]);
     return this.compare(version) === 0;
   }
   /**
    * @description 不等于
-   * @param {String|Version} version 版本号，如1.0.0或Version实例
+   * @param {String|Version} version 版本号，如1.0.0或Version实例，必传
    * @returns {Boolean} 返回比较结果，若实例版本不等于参数版本，返回true，否则返回false
    */
   neq(version) {
+    checkRTAOrError(version, "version", true, [["String", Version]]);
     return this.compare(version) !== 0;
   }
   /**
    * @description 大于等于
-   * @param {String|Version} version 版本号，如1.0.0或Version实例
+   * @param {String|Version} version 版本号，如1.0.0或Version实例，必传
    * @returns {Boolean} 返回比较结果，若实例版本大于等于参数版本，返回true，否则返回false
    */
   gte(version) {
+    checkRTAOrError(version, "version", true, [["String", Version]]);
     return this.compare(version) >= 0;
   }
   /**
    * @description 小于等于
-   * @param {String|Version} version 版本号，如1.0.0或Version实例
+   * @param {String|Version} version 版本号，如1.0.0或Version实例，必传
    * @returns {Boolean} 返回比较结果，若实例版本小于等于参数版本，返回true，否则返回false
    */
   lte(version) {
+    checkRTAOrError(version, "version", true, [["String", Version]]);
     return this.compare(version) <= 0;
   }
-
   /**
    * @description 比较方法（静态）
-   * @param {String|Version} version1 版本号1，如1.0.0或Version实例
-   * @param {String|Version} version2 版本号2，如1.0.0或Version实例
+   * @param {String|Version} version1 版本号1，如1.0.0或Version实例，必传
+   * @param {String|Version} version2 版本号2，如1.0.0或Version实例，必传
    * @returns {Number} 返回比较结果，若版本号1大于版本号2，返回1；相等，返回0；小于，返回-1
    */
   static compare(version1, version2) {
+    checkRTAOrError(version1, "version1", true, [["String", Version]]);
+    checkRTAOrError(version2, "version2", true, [["String", Version]]);
     const ver1 = (version1 =
       version1 instanceof Version ? version1 : new Version(version1));
     return ver1.compare(version2);
   }
-
   /**
    * @description 任意比较符号比较
-   * @param {String|Version} version1 版本号1，如1.0.0或Version实例
-   * @param {String} symbol 比较符号，如>、<、>=、lt、gte等
-   * @param {String|Version} version2 版本号2，如1.0.0或Version实例
+   * @param {String|Version} version1 版本号1，如1.0.0或Version实例，必传
+   * @param {String} symbol 比较符号，如>、<、>=、lt、gte等，必传
+   * @param {String|Version} version2 版本号2，如1.0.0或Version实例，必传
    * @returns {Boolean} 返回比较结果，若版本号1和版本号2在对应符号意义的比较上成立，返回true，否则返回false
    */
   static compareWith(version1, symbol, version2) {
+    checkRTAOrError(version1, "version1", true, [["String", Version]]);
+    checkRTAOrError(symbol, "symbol", true, ["String"]);
+    checkRTAOrError(version2, "version2", true, [["String", Version]]);
     const ver1 = version1 instanceof Version ? version1 : new Version(version1);
     return ver1.compareWith(symbol, version2);
   }
